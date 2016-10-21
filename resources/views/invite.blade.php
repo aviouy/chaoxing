25b5c2e44f071f40f00b1fe8d7c20c2f994cfc6d
aviouy <aviouy@163.com>
2016-10-21 11:28:02 +0800

    邀请页面布局

---
+++ resources/views/invite.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>教你如何网络课100分</title>
+
+    <!-- Bootstrap -->
+	  <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js "></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<div class="row clearfix">
+				<div class="col-md-12 column">
+					<div class="page-header">
+						<h1>
+							抬头
+						</h1>
+					</div>
+				</div>
+			</div>
+			<div class="jumbotron">
+				<h1>
+					Hello, world!
+				</h1>
+				<p>
+					轮播框
+				</p>
+			</div>
+			<div class="row clearfix">
+				<div class="col-md-12 column">
+					<img alt="140x140" src="v3/default3.jpg" />
+					
+					<p>
+						 长按二维码...........
+					</p>
+				</div>
+			</div>
+			<div class="row clearfix">
+				<div class="col-md-12 column">
+					 <button type="button" class="btn btn-default btn-block">让大家一起100分</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+
+
+
+	<script src="http://apps.bdimg.com/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
